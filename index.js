46c96bbec0877bbc9b92b15989165b8e614fc1b2
Guilherme Novais Silva <guilherme.silva@grupoicr.local>
2025-02-01 20:48:08 -0300

    Criando metodos(funcoes) para manipularmos nosso objeto, utilizando o 'this' para referenciar que o atributo que estamos nos referindo é oq esta dentro da classe

--- index.js
+++ index.js
@@ -16,25 +16,50 @@
 class Cliente { //classe || model
     nome;//definindo atributos
     cpf;
+}
+
+
+class ContaCorrente {
     agencia;
     saldo;
+
+
+    sacar(valor){ //criando um metodo || funcao 
+        if (this.saldo >= valor){ // this aponta que o atributo que devemos utilizar é o do objeto que construimos
+            this.saldo -= valor
+        }else{
+            console.log("valor de saque acima do saldo atual da conta")
+        }
+    
+    }
+
 }
+
+
 //utilizando classe para criar um novo  objeto
 const cliente0 = new Cliente(); //objeto || instancia da classe()
-const cliente1 = new Cliente(); 
-
 cliente0.nome = 'Ricardo';
 cliente0.cpf = 111122233309;
-cliente0.agencia = 1001;
-cliente0.saldo = 0;
 
 
+const cliente1 = new Cliente(); 
 cliente1.nome = 'Alice';
 cliente1.cpf = 948145614816;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
+
+
+const contaCorrenteCliente0 = new ContaCorrente();
+contaCorrenteCliente0.agencia = 1001;
+contaCorrenteCliente0.saldo = 50;
+contaCorrenteCliente0.sacar(100);// utilizando metodo para efetuar o saque
+
+
+const contaCorrenteCliente1 = new ContaCorrente();
+contaCorrenteCliente1.agencia = 1001;
+contaCorrenteCliente1.saldo = 0;
 
 
 
-console.log("Cliente 0:",cliente0)//exibe o objeto inteiro, com base no atributos que foram definidos
-console.log("Cliente 1:",cliente1)
+
+
+console.log("Cliente 0:",cliente0, "informações da conta:", contaCorrenteCliente0);//exibe o objeto inteiro, com base no atributos que foram definidos
+console.log("Cliente 1:",cliente1, "informações da conta:", contaCorrenteCliente1);
